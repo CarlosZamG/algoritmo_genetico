c9b7a14405332b7fb0a03f13cd46b540ea392432
Carlos Zamora <cdzamora02@outlook.es>
2024-07-23 13:43:08 -0600

    feat(main): change to dark background

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def run():
+    
+    plt.style.use("dark_background")
     f = lambda x: np.cos(x)
     limit_1 = -7
     limit_2 = 7
     roots = find_roots(f, limit_1, limit_2, 50, 8)
     fig, ax = plt.subplots()
     xplot = np.linspace(limit_1, limit_2)
-    plt.grid()
+    ax.grid(True)
     ax.plot(xplot, f(xplot))
-    ax.scatter(roots, f(roots), c = roots.size*[3])
+    ax.scatter(roots, f(roots), c = "xkcd:salmon")
     plt.savefig("graph.svg")
     plt.close()
 
